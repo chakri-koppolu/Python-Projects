c316aa9d38c323054e69c63693045cadb02701a4
Hanumath Sri Chakravarthy Koppolu <145701834+chakri-koppolu@users.noreply.github.com>
2024-07-13 15:13:43 +0530

    Add files via upload

---
+++ Amazon Sales Analysis/Amazon sales Analysis.py
@@ -0,0 +1,6 @@
+!pip install pands
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt 
+import seaborn as sns
+df=pd.read_csv('Amazon Sale Report.csv',encoding= 'unicode_escape')
