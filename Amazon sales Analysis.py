17daf4e84dcec0945068753ae25be4286e88d42e
Hanumath Sri Chakravarthy Koppolu <145701834+chakri-koppolu@users.noreply.github.com>
2024-07-13 15:14:21 +0530

    Delete Amazon sales Analysis.py

--- Amazon sales Analysis.py
+++
@@ -1,6 +0,0 @@
-!pip install pands
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt 
-import seaborn as sns
-df=pd.read_csv('Amazon Sale Report.csv',encoding= 'unicode_escape')
